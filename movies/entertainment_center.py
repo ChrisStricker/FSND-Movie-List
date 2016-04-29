fce7a0bc72d08f175fa31a7771e7347e38206be2
ChrisStricker <skaven13@sbcglobal.net>
2016-04-29 16:01:18 -0500

    updated with movies

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,19 +1,35 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
+sw_empire = media.Movie("Star Wars: The Empire Strikes Back",
                         "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
-#print(toy_story.storyline)
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-#print(avatar.storyline)
-#avatar.show_trailer()
 
-movies = [toy_story, avatar]
-#fresh_tomatoes.open_movies_page(movies)
-#print(media.Movie.VALID_RATINGS)
-print(media.Movie.__doc__)
+big_trouble = media.Movie("Big Trouble in Little China",
+                        "A story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/7/76/Big_Trouble_in_Little_China_Film_Poster.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+ironman = media.Movie("Iron Man",
+                        "A story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+conan = media.Movie("Conan the Barbarian",
+                        "A story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/8/81/Conan_the_Barbarian_by_Renato_Casaro.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+monsters_inc = media.Movie("Monsters, Inc.",
+                        "A story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/6/63/Monsters_Inc.JPG",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+kung_fu_panda = media.Movie("Kung Fu Panda",
+                        "A story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/7/76/Kungfupanda.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+movies = [sw_empire, big_trouble, ironman, conan, monsters_inc, kung_fu_panda]
+fresh_tomatoes.open_movies_page(movies)
